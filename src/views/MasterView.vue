b0ee66693fe94d0525f7b77dc00337e3a2801d0d
Yuuki Wesp <ls-micro@ya.ru>
2025-05-08 12:39:40 +0300

    finally

--- src/views/MasterView.vue
+++ src/views/MasterView.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="app-container flex h-screen gap-4 p-7 flex items-center justify-center min-h-screen"
     style="width: 100svw; height: 100svh;">
-    <span style="position: fixed;top: 5rem;font-size: 4rem;">Argon Chat</span>
+    <span style="position: fixed; top: 5rem;     min-width: 120rem;">
+      <TextHoverEffect class="max-lg:w-full min-md:flex-1" text="Argon Chat" />
+    </span>
 
-    <div v-if="!isSuccessLink">
+    <div v-if="!isCompletedPhase">
       <div class="flex flex-col items-center gap-6 text-center" v-if="avatarBlock && authKey">
         <br />
         <div class="flex items-center gap-4 text-4xl text-white/80">
@@ -26,7 +28,8 @@
       </div>
     </div>
     <div v-else>
-      <div class="flex items-center gap-4 text-4xl text-white/80" style="    flex-direction: column;">
+
+      <div class="flex items-center gap-4 text-4xl text-white/80" style="flex-direction: column;" v-if="isSuccessLink">
         <div>
           <img :src="sse" style="width: 10rem;" />
         </div>
@@ -34,6 +37,14 @@
         <Sparkness text="Success linked" :colors="{ first: '#9E7AFF', second: '#FE8BBB' }" :sparkles-count="10"
           class="my-8" />
         <Confetti ref="confettiRef" class="absolute left-0 top-0 z-0 size-full" @mouseenter="fireConfetti" />
+      </div>
+      <div v-else>
+        <div class="flex items-center gap-4 text-4xl text-white/80" style="flex-direction: column;">
+          <div>
+            <img :src="sss" style="width: 10rem;" />
+          </div>
+          Unknown error, please try again later ☹️
+        </div>
       </div>
     </div>
 
@@ -56,6 +67,9 @@ import sss from "@/assets/undraw_farming_u62j.svg";
 import sse from "@/assets/undraw_zoom-in_miok.svg";
 import Confetti from '@/components/Confetti.vue';
 import Sparkness from "@/components/SparklesText.vue";
+import TextHoverEffect from '@/components/TextHoverEffect.vue';
+
+import { createTransportClient } from "@argon-chat/argx";
 
 const brd = basex("123qpxcvd");
 
@@ -63,6 +77,10 @@ const confettiRef = ref(null as any);
 
 function fireConfetti() {
   confettiRef.value?.fire({});
+}
+
+interface IUserInteraction {
+  CreateSocialBoundAsync(request: { TgUser: string, Token: string, Kind: "Telegram", UserSlash: string }): Promise<boolean>;
 }
 
 const rrd = {
@@ -75,19 +93,24 @@ const rrd = {
 const avatarBlock = useRouteQuery('aav', "", { transform: { get: rrd.decode, set: rrd.encode } });
 const authKey = useRouteQuery("aac", "", { transform: { get: rrd.decode, set: rrd.encode } });
 
+const isCompletedPhase = ref(false);
 const isSuccessLink = ref(false);
 
-onMounted(() => {
-  logger.log("AvatarBlock", avatarBlock.value);
-})
-
-
+const client = createTransportClient("https://api.argon.gl", () => "");
+const handleUserScope = client.scope<IUserInteraction>("IUserInteraction");
 const avatarUrl = computed(() => `https://xcdn.argon.gl/user/${avatarBlock.value}.jpg`);
 
 
-function handleUserAuth(user: LoginWidgetUser) {
+async function handleUserAuth(user: LoginWidgetUser) {
   logger.log(user);
-  isSuccessLink.value = true;
+  isCompletedPhase.value = true;
+
+  isSuccessLink.value = await handleUserScope.CreateSocialBoundAsync({
+    Token: authKey.value,
+    TgUser: JSON.stringify(user),
+    Kind: "Telegram",
+    UserSlash: avatarBlock.value
+  })
 }
 
 </script>
